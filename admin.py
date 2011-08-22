ee3138b416887cfa6a67c09291eef17eeada4557
Yaşar Arabacı <yasar11732@gmail.com>
2011-08-22 23:16:19 +0300

    removed my_slugify

--- admin.py
+++ admin.py
@@ -5,26 +5,6 @@ from django.contrib import admin
 from datetime import datetime
 
 # To handle turkish characters better!
-def my_slugify(text):
-    change_characters = {
-        u"ı" : u"i",
-        u"İ" : u"i",
-        u"ğ" : u"ğ",
-        u"Ğ" : u"Ğ",
-        u"ü" : u"u",
-        u"Ü" : u"U",
-        u"ş" : u"s",
-        u"Ş" : u"S",
-        u"ö" : u"o",
-        u"Ö" : u"O",
-        u"ç" : u"c",
-        u"Ç" : u"C"
-        
-    }
-    
-    for k, v in change_characters.items():
-        text.replace(k,v)
-    return slugify(text)
 
 class PostAdmin(admin.ModelAdmin):
     readonly_fields = ("slug","last_mod","pub_date")
@@ -42,7 +22,7 @@ class PostAdmin(admin.ModelAdmin):
     
     def save_model(self,request,obj,form,change):
         if obj.slug == "" or obj.slug is None:
-            obj.slug = my_slugify(obj.title)
+            obj.slug = slugify(obj.title)
         # I get database representation of the object to check if
         # it was not published before and it is published now
         # in order to set pub_date right
